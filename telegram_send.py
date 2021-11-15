43461e503bc4876c03a1794d440fc07c3517328e
orel8654 <egorao@mail.ru>
2021-11-15 18:54:22 +0700

    TC

--- telegram_send.py
+++ telegram_send.py
@@ -1,8 +1,8 @@
 import requests
-
+from config import TELEGRAM_TOKEN
 def send_message(text):
 
-    api_token = '2076509234:AAEx0X5agqD1rkJxiOK8rFVIZbC4dWaNsHc'
+    api_token = TELEGRAM_TOKEN
 
     requests.get('https://api.telegram.org/bot{}/sendMessage'.format(api_token), params=dict(
         chat_id='@sendprocessing',
