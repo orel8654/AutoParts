37b39642713267619394f86fc6ed4f754590b866
orel8654 <egorao@mail.ru>
2021-11-19 04:51:26 +0700

    update

--- amayama_parser_additionally.py
+++ amayama_parser_additionally.py
@@ -33,9 +33,32 @@ def get_content(html):
     })
     return lst_dict
 
-def main(url):
-    html = get_html(url).text
-    get_content(html)
+def get_content_spec(html):
+    lst_dict = []
+    soup = BeautifulSoup(html, 'lxml')
+    try:
+        items = soup.find('div', class_='list')
+        all_links = items.find_all('a')
+        for i in all_links:
+            link = i.get('href')
+            lst_dict.append('https://www.amayama.com' + link)
+        return lst_dict
+    except:
+        return None
+
+def main(car_mark, car_model):
+    list_links = []
+    all_categories = []
+    all_acces = []
+    html = get_html(f'https://www.amayama.com/ru/catalogs/{car_mark}/{car_model}').text
+    check = get_content_spec(html)
+    if check != None:
+        list_links.extend(check)
+        for i in list_links:
+            html = get_html(i).text
+            all_categories.extend(get_content(html))
+    else:
+        print('Информации о машине не найдено!')
 
 if __name__ == '__main__':
-    main('https://www.amayama.com/ru/catalogs/nissan/elgrand/1-van-3-right-e50-2000-r-1546/body-parts-6/hood-panel-hinge-fitting-body-571')
+    main(car_mark='nissan', car_model='elgrand')
