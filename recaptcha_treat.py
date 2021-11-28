25e844a4a647f002fb8e22f482ad064b10b5b259
orel8654 <egorao@mail.ru>
2021-11-29 00:09:26 +0700

    ADD REGEX UPDATE

---
+++ recaptcha_treat.py
@@ -0,0 +1,10 @@
+import recaptcha
+
+def input_main(data):
+    car_mark = data['car_mark'].split(',')[0]
+    part_number = data['car_mark'].split(',')[-1]
+    return recaptcha.find_past(part_number, car_mark)
+
+
+# if __name__ == '__main__':
+#     input_main({'car_mark': 'toyota, 52119-13340-A0'})
