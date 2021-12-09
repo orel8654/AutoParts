5a509de9bc5b3ba613db714976a8d5c667b0bc27
orel8654 <egorao@mail.ru>
2021-12-10 06:30:46 +0700

    ASYNC

--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -19,7 +19,7 @@ logging.basicConfig(level=logging.INFO)
 loop = asyncio.get_event_loop()
 bot = Bot(token=TELEGRAM_TOKEN_BOT_GENERAL)
 dp = Dispatcher(bot, storage=MemoryStorage())
-p2p = QiwiP2P(auth_key=QIWI_SECRET_KEY, default_amount=990)
+p2p = QiwiP2P(auth_key=QIWI_SECRET_KEY, default_amount=345)
 
 '''----------------------------Машина состояния----------------------------'''
 class Form(StatesGroup):
@@ -35,6 +35,11 @@ class RCA(StatesGroup):
 
 class Pay(StatesGroup):
     data = State()
+
+class Report(StatesGroup):
+    vin = State()
+    data = State()
+    mark = State()
 
 class Executor:
     """In most cases, you can just use the 'execute' instance as a
@@ -63,7 +68,7 @@ async def check_pay(bill):
 @dp.message_handler(commands=['start'])
 async def command_start(message:types.Message):
     if message.chat.type == 'private':
-        await bot.send_message(message.from_user.id, 'Привет, {0.first_name}!\nЯ помогу тебе получить информацию о ценах запчастей!\n/help - инструкция по использованию бота!\n/info - дополнительная информация о проекте!\n/supmod - каталог моделей'.format(message.from_user), reply_markup=markups.start_btn)
+        await bot.send_message(message.from_user.id, 'Привет, {0.first_name}!\n\nБот предоставляет актуальные цены на запчасти из открытых источников AMAYAMA, EMEX и РСА\n\n/help - инструкция по использованию бота!\n/info - дополнительная информация!\n/supmod - каталог моделей!\n/pay - информация об оплате!'.format(message.from_user), reply_markup=markups.start_btn)
 
 @dp.message_handler(commands=['help'])
 async def command_help(message:types.Message):
@@ -76,6 +81,10 @@ async def command_info(message:types.Message):
 @dp.message_handler(commands=['supmod'])
 async def command_info(message:types.Message):
     await bot.send_message(message.from_user.id, f'{markups.CMD_SUPMOD}')
+
+@dp.message_handler(commands=['pay'])
+async def command_pay(message:types.Message):
+    await bot.send_message(message.from_user.id, f'{markups.CMD_PAY}')
 
 '''----------------------------Для DEBUG, тесты, проверки----------------------------'''
 @dp.message_handler(commands=['admintest'])
@@ -90,58 +99,58 @@ async def echo_send(message: types.Message):
 
         if message.text == 'Выбор функций':
             if db.user_id_check(message.from_user.id) == False:
-                await bot.send_message(message.from_user.id, 'Вы новый пользователь! Хотите оформить подписку?', reply_markup=markups.subs_btn)
+                await bot.send_message(message.from_user.id, 'Вы новый пользователь! Оформите подписку!', reply_markup=markups.subs_btn)
             else:
                 if db.check_days_subs(message.from_user.id) == False:
-                    await bot.send_message(message.from_user.id, 'У вас закончилась подписка! Хотите продлить?', reply_markup=markups.continue_subs_btn)
+                    await bot.send_message(message.from_user.id, 'У вас закончилась подписка! продлите подписку!', reply_markup=markups.continue_subs_btn)
                 else:
                     await bot.send_message(message.from_user.id, 'Аутентификация пройдена!', reply_markup=markups.activate_program)
 
 
         elif message.text == 'Оформить подписку':
             if message.chat.type == 'private':
-                new_bill = p2p.bill(bill_id=message.from_user.id, lifetime=5, comment='APT')
-                await bot.send_message(message.from_user.id, f'Стоимость подписки 990 рублей!\nВаша ссылка на оплату:\n{new_bill.pay_url}\n\nСсылка активна 5 минут!')
-                await asyncio.sleep(300)
+                new_bill = p2p.bill(bill_id=message.from_user.id + random.randint(1000, 9999), lifetime=2, comment='APT')
+                await bot.send_message(message.from_user.id, f'Стоимость подписки 345 рублей!\nВаша ссылка на оплату:\n{new_bill.pay_url}\n\nСсылка активна 2 минуты!')
+                await asyncio.sleep(120)
                 check = await check_pay(new_bill)
                 if check != 'WAITING':
                     if db.user_add(message.from_user.id) == True:
-                        await bot.send_message(message.from_user.id, 'Подписка успешна добавлена!', reply_markup=markups.start_btn)
+                        await bot.send_message(message.from_user.id, 'Подписка успешна оформлена!', reply_markup=markups.start_btn)
                     else:
                         await bot.send_message(message.from_user.id, 'Произошла ошибка! Напишите пожалуйста, администратору для решения проблемы!')
                 else:
-                    await bot.send_message(message.from_user.id, 'Ваш счет закрыт! Повторите попытку!')
+                    await bot.send_message(message.from_user.id, 'Ваш счет оплаты закрыт! Повторите попытку!')
                     p2p.reject(bill_id=new_bill.bill_id)
 
 
         elif message.text == 'Продлить подписку':
             if message.chat.type == 'private':
-                new_bill = p2p.bill(bill_id=message.from_user.id, lifetime=5, comment='APT')
-                await bot.send_message(message.from_user.id, f'Стоимость подписки 990 рублей!\nВаша ссылка на оплату:\n\n{new_bill.pay_url}\n\nСсылка активна 5 минут!')
-                await asyncio.sleep(300)
+                new_bill = p2p.bill(bill_id=message.from_user.id + random.randint(1000, 9999), lifetime=2, comment='APT')
+                await bot.send_message(message.from_user.id, f'Стоимость подписки 345 рублей!\nВаша ссылка на оплату:\n\n{new_bill.pay_url}\n\nСсылка активна 2 минуты!')
+                await asyncio.sleep(120)
                 check = await check_pay(new_bill)
                 if check != 'WAITING':
                     if db.user_update(message.from_user.id) == True:
-                        await bot.send_message(message.from_user.id, 'Ваша подписка успешна продлена!', reply_markup=markups.start_btn)
+                        await bot.send_message(message.from_user.id, 'Ваша подписка успешна оформлена!', reply_markup=markups.start_btn)
                     else:
                         await bot.send_message(message.from_user.id, 'Произошла ошибка! Напишите пожалуйста, администратору для решения проблемы!')
                 else:
-                    await bot.send_message(message.from_user.id, 'Ваш счет закрыт! Повторите попытку!')
+                    await bot.send_message(message.from_user.id, 'Ваш счет оплаты закрыт! Повторите попытку!')
                     p2p.reject(bill_id=new_bill.bill_id)
 
         elif message.text == 'Главная':
-            await bot.send_message(message.from_user.id, 'Какие действия сделать?', reply_markup=markups.activate_program)
+            await bot.send_message(message.from_user.id, 'Выберите функцию?', reply_markup=markups.activate_program)
 
 
         elif message.text == 'Выборка по модели':
             if db.user_id_check(message.from_user.id) == True:
                 if db.check_days_subs(message.from_user.id) == True:
-                    await bot.send_message(message.from_user.id, 'Введите марку, модель, год начала производства автомобиля через запятую, например (toyota, allex, 2001)', reply_markup=markups.input_btn)
+                    await bot.send_message(message.from_user.id, 'Введите марку, модель, год начала производства автомобиля через запятую. Пример - toyota, allex, 2001', reply_markup=markups.input_btn)
                     await Form.general.set()
                 else:
-                    await bot.send_message(message.from_user.id, 'у вас закончилась подписка! Хотите продлить?', reply_markup=markups.btn_continue_subs)
+                    await bot.send_message(message.from_user.id, 'У вас закончилась подписка! Хотите продлить?', reply_markup=markups.btn_continue_subs)
             else:
-                await bot.send_message(message.from_user.id, 'Вы новый пользователь, пожалуйста оформите подписку!', reply_markup=markups.subs_btn)
+                await bot.send_message(message.from_user.id, 'Вы новый пользователь, оформите подписку!', reply_markup=markups.subs_btn)
 
 
         elif message.text == 'Выборка по цене':
@@ -152,16 +161,72 @@ async def echo_send(message: types.Message):
         elif message.text == 'Расчет РСА':
             if db.user_id_check(message.from_user.id) == True:
                 if db.check_days_subs(message.from_user.id) == True:
-                    await bot.send_message(message.from_user.id, 'Введите марку авто и номер детали через запятую, например (toyota, 52119-13340-A0). Расчет может занять до 2 минут!')
+                    await bot.send_message(message.from_user.id, 'Введите марку авто, номер детали через запятую. Пример - toyota, 52119-13340-A0. Расчте может занять от 30 до 120 секунд!')
                     await RCA.car_mark.set()
                 else:
                     await bot.send_message(message.from_user.id, 'У вас закончилась подписка! Хотите продлить?', reply_markup=markups.btn_continue_subs)
             else:
-                await bot.send_message(message.from_user.id, 'Вы новый пользователь, пожалуйста оформите подписку!', reply_markup=markups.subs_btn)
+                await bot.send_message(message.from_user.id, 'Вы новый пользователь, оформите подписку!', reply_markup=markups.subs_btn)
 
         elif message.text == 'Админ':
             if db.admin_add(message.from_user.id) == True:
                 await bot.send_message(message.from_user.id, 'Режим администратора активирован!', reply_markup=markups.start_btn)
+
+
+        elif message.text == 'Сделать отчет':
+            if db.user_id_check(message.from_user.id) == True:
+                if db.check_days_subs(message.from_user.id) == True:
+                    await bot.send_message(message.from_user.id, 'Отправьте номер кузова авто!')
+                    await Report.vin.set()
+                else:
+                    await bot.send_message(message.from_user.id, 'У вас закончилась подписка! Хотите продлить?', reply_markup=markups.btn_continue_subs)
+            else:
+                await bot.send_message(message.from_user.id, 'Вы новый пользователь, оформите подписку!', reply_markup=markups.subs_btn)
+
+
+
+'''----------------------------Обработчик машины состояния для Сделать отчет----------------------------'''
+@dp.message_handler(state=Report.vin)
+async def RSA_report(message: types.Message, state: FSMContext):
+    import report_file_rsa
+    import report_file_rsa_with_parse
+
+
+    if db.check_days_subs(message.from_user.id) == True:
+        async with state.proxy() as proxy:
+            if message.text != 'Главная':
+                await write.write_request_from_user(message.from_user.id, message.text, 'ОТЧЕТ')
+                proxy['vin'] = message.text
+                try:
+                    data, car_mark = await report_file_rsa.main(proxy)
+                except:
+                    car_mark = ''
+                if car_mark != '':
+                    new_bill_report = p2p.bill(bill_id=message.from_user.id + random.randint(1000, 9999), amount=20, lifetime=2, comment='APTreport')
+                    await bot.send_message(message.from_user.id, f'Мы собрали информацию по автомобилю:\n\n{car_mark.upper()}\n\n№{message.text.upper()}\n\nКоличество запчастей: {len(data)}\n\nКупить отчет по ссылке: {new_bill_report.pay_url}\nСылка активна 2 минуты! После проверки оплаты мы пришлем вам отчет!', reply_markup=markups.back_back_btn2)
+                    await asyncio.sleep(120)
+                    check = await check_pay(new_bill_report)
+                    if check != 'WAITING':
+                        await bot.send_message(message.from_user.id, 'Оплата прошла успешно! Ожидайте!', reply_markup=markups.back_back_btn2)
+                        try:
+                            await message.answer_document(open(f'reports_rsa/{message.text.upper()}.txt', 'rb'))
+                        except Exception:
+                            await report_file_rsa.callback_pars(data, car_mark, message.text)
+                            await message.answer_document(open(f'reports_rsa/{message.text.upper()}.txt', 'rb'))
+                    else:
+                        await bot.send_message(message.from_user.id, f'Счет оплаты {message.text.upper()} истек! Повторите попытку!', reply_markup=markups.back_back_btn2)
+                        p2p.reject(bill_id=new_bill_report.bill_id)
+                else:
+                    '''
+                    Здесь добавить парсинг не найденных файлов автомобилей таких как Nissan-europe, Nissan-japan, Toyota-europe, Toyota-japan
+                    '''
+                    await bot.send_message(message.from_user.id, f'Информации по номеру\n{message.text.upper()}\nНе найдено!',reply_markup=markups.back_back_btn2)
+            else:
+                await bot.send_message(message.from_user.id, 'Подтвердите!', reply_markup=markups.back_back_btn2)
+                await state.finish()
+    else:
+        await bot.send_message(message.from_user.id, 'У вас закончилась подписка! Хотите продлить?', reply_markup=markups.btn_continue_subs)
+        await state.finish()
 
 
 
@@ -207,7 +272,7 @@ async def processing_text(message: types.Message, state: FSMContext):
                 await bot.send_message(message.from_user.id, 'Подтвердите!', reply_markup=markups.back_back_btn2)
                 await state.finish()
     else:
-        await bot.send_message(message.from_user.id, 'У вас закончилась подписка!', reply_markup=markups.btn_continue_subs)
+        await bot.send_message(message.from_user.id, 'У вас закончилась подписка! Хотите продлить?', reply_markup=markups.btn_continue_subs)
         await state.finish()
 
 
@@ -233,7 +298,7 @@ async def processing_text_category(message: types.Message, state: FSMContext):
                     await bot.send_message(message.from_user.id, 'Запчастей не найдено!')
                     await write.write_answer_from_functions(message.from_user.id, 'Запчастей не найдено!', 'КАТЕГОРИИ')
             else:
-                await bot.send_message(message.from_user.id, 'У вас закончилась подписка!', reply_markup=markups.back_back_btn2)
+                await bot.send_message(message.from_user.id, 'У вас закончилась подписка! Хотите продлить?', reply_markup=markups.back_back_btn2)
                 await state.finish()
 
 
@@ -258,7 +323,7 @@ async def processing_text_category(message: types.Message, state: FSMContext):
                     await bot.send_message(message.from_user.id, 'Запчастей не найдено!')
                     await write.write_answer_from_functions(message.from_user.id, 'Запчастей не найдено!', 'КАТЕГОРИИ')
             else:
-                await bot.send_message(message.from_user.id, 'У вас закончилась подписка!', reply_markup=markups.back_back_btn2)
+                await bot.send_message(message.from_user.id, 'У вас закончилась подписка! Хотите продлить?', reply_markup=markups.back_back_btn2)
                 await state.finish()
 
 
@@ -282,7 +347,7 @@ async def processing_text_category(message: types.Message, state: FSMContext):
                     await bot.send_message(message.from_user.id, 'Запчастей не найдено!')
                     await write.write_answer_from_functions(message.from_user.id, 'Запчастей не найдено!', 'КАТЕГОРИИ')
             else:
-                await bot.send_message(message.from_user.id, 'У вас закончилась подписка!', reply_markup=markups.back_back_btn2)
+                await bot.send_message(message.from_user.id, 'У вас закончилась подписка! Хотите продлить?', reply_markup=markups.back_back_btn2)
                 await state.finish()
 
 
@@ -306,7 +371,7 @@ async def processing_text_category(message: types.Message, state: FSMContext):
                     await bot.send_message(message.from_user.id, 'Запчастей не найдено!')
                     await write.write_answer_from_functions(message.from_user.id, 'Запчастей не найдено!', 'КАТЕГОРИИ')
             else:
-                await bot.send_message(message.from_user.id, 'У вас закончилась подписка!', reply_markup=markups.back_back_btn2)
+                await bot.send_message(message.from_user.id, 'У вас закончилась подписка! Хотите продлить?', reply_markup=markups.back_back_btn2)
                 await state.finish()
 
 
@@ -330,7 +395,7 @@ async def processing_text_category(message: types.Message, state: FSMContext):
                     await bot.send_message(message.from_user.id, 'Запчастей не найдено!')
                     await write.write_answer_from_functions(message.from_user.id, 'Запчастей не найдено!', 'КАТЕГОРИИ')
             else:
-                await bot.send_message(message.from_user.id, 'У вас закончилась подписка!', reply_markup=markups.back_back_btn2)
+                await bot.send_message(message.from_user.id, 'У вас закончилась подписка! Хотите продлить?', reply_markup=markups.back_back_btn2)
                 await state.finish()
 
 
@@ -354,7 +419,7 @@ async def processing_text_category(message: types.Message, state: FSMContext):
                     await bot.send_message(message.from_user.id, 'Запчастей не найдено!')
                     await write.write_answer_from_functions(message.from_user.id, 'Запчастей не найдено!', 'КАТЕГОРИИ')
             else:
-                await bot.send_message(message.from_user.id, 'У вас закончилась подписка!', reply_markup=markups.back_back_btn2)
+                await bot.send_message(message.from_user.id, 'У вас закончилась подписка! Хотите продлить?', reply_markup=markups.back_back_btn2)
                 await state.finish()
 
 
@@ -378,7 +443,7 @@ async def processing_text_category(message: types.Message, state: FSMContext):
                     await bot.send_message(message.from_user.id, 'Запчастей не найдено!')
                     await write.write_answer_from_functions(message.from_user.id, 'Запчастей не найдено!', 'КАТЕГОРИИ')
             else:
-                await bot.send_message(message.from_user.id, 'У вас закончилась подписка!', reply_markup=markups.back_back_btn2)
+                await bot.send_message(message.from_user.id, 'У вас закончилась подписка! Хотите продлить?', reply_markup=markups.back_back_btn2)
                 await state.finish()
 
 
@@ -402,7 +467,7 @@ async def processing_text_category(message: types.Message, state: FSMContext):
                     await bot.send_message(message.from_user.id, 'Запчастей не найдено!')
                     await write.write_answer_from_functions(message.from_user.id, 'Запчастей не найдено!', 'КАТЕГОРИИ')
             else:
-                await bot.send_message(message.from_user.id, 'У вас закончилась подписка!', reply_markup=markups.back_back_btn2)
+                await bot.send_message(message.from_user.id, 'У вас закончилась подписка! Хотите продлить?', reply_markup=markups.back_back_btn2)
                 await state.finish()
 
 
